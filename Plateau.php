4ad07bfadce540c503183befd50e9317f6fcf19d
Igorko <gryzzbox@gmail.com>
2016-01-24 16:01:54 +0200

    Extended isMovePossible method, added more tests.

--- Plateau.php
+++ Plateau.php
@@ -4,6 +4,13 @@ include_once "Rover.php";
 
 class Plateau {
 
+    const NORTH = 'N';
+    const SOUTH = 'S';
+    const WEST = 'W';
+    const EAST = 'E';
+    const COORDINATE_Y = 'y';
+    const COORDINATE_X = 'x';
+
     /**
      * @var int
      */
@@ -14,6 +21,26 @@ class Plateau {
      */
     private $sizeY;
 
+    private $movingMap = [
+        self::NORTH => [
+            'coordinate' => self::COORDINATE_Y,
+            'alteration' => 1
+        ],
+        self::SOUTH => [
+            'coordinate' => self::COORDINATE_Y,
+            'alteration' => -1
+        ],
+        self::EAST => [
+            'coordinate' => self::COORDINATE_X,
+            'alteration' => 1
+        ],
+        self::WEST => [
+            'coordinate' => self::COORDINATE_X,
+            'alteration' => -1
+        ],
+
+    ];
+
     public function __construct($x, $y) {
         $this->sizeX = $x;
         $this->sizeY = $y;
@@ -22,12 +49,30 @@ class Plateau {
     /**
      * Checks if move to direction is possible from defined coordinates;
      *
-     * @param int $x
-     * @param int $y
+     * @throws Exception
+     * @param int $xCoordinate
+     * @param int $yCoordinate
      * @param string $orientation
      * @return bool
      */
     public function isMovePossible($xCoordinate, $yCoordinate, $orientation) {
+
+        if ($xCoordinate > $this->sizeX || $xCoordinate < 0 || $yCoordinate > $this->sizeY || $yCoordinate < 0) {
+            throw new Exception('Coordinates are out of range.');
+        }
+        $alteration = $this->movingMap[$orientation];
+
+        if ($alteration['coordinate'] == self::COORDINATE_X) {
+            $newXCoordinate = $xCoordinate + $alteration['alteration'];
+            if ($newXCoordinate > $this->sizeX || $newXCoordinate < 0) {
+                return false;
+            }
+        } else {
+            $newYCoordinate = $yCoordinate + $alteration['alteration'];
+            if ($newYCoordinate > $this->sizeY || $newYCoordinate < 0) {
+                return false;
+            }
+        }
         return true;
     }
 
--- tests/Plateau.php
+++ tests/Plateau.php
@@ -7,6 +7,17 @@ class PlateauTest extends PHPUnit_Framework_TestCase {
     public function testIsMovePossible() {
         $plateau = new Plateau(5, 5);
 
-        $this->assertTrue($plateau->isMovePossible(1,1, 'N'));
+        $this->assertTrue($plateau->isMovePossible(1, 1, 'N'));
+        $this->assertFalse($plateau->isMovePossible(0, 0, 'W'));
+        $this->assertTrue($plateau->isMovePossible(0, 0, 'E'));
+        $this->assertFalse($plateau->isMovePossible(5, 0, 'S'));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testIsMovePossibleException() {
+        $plateau = new Plateau(5, 5);
+        $this->assertTrue($plateau->isMovePossible(1, 10, 'N'));
     }
 }
