c1222773dd28899a2a54abc00a325bffa54858a0
Igorko <gryzzbox@gmail.com>
2016-01-24 17:44:25 +0200

    Added logic to execution file.

--- goRovers.php
+++ goRovers.php
@@ -1,17 +1,36 @@
 <?php
 
+include_once __DIR__ . "/Plateau.php";
+include_once __DIR__ . "/Rover.php";
+
 $plateauSize = [];
-$roversInput = [];
+$roversInputs = [];
 
 $handle = fopen("input.txt", "r");
 while (($line = fgets($handle)) !== false) {
     if (!$plateauSize) {
         list($plateauSize['x'], $plateauSize['y']) = explode(' ', trim($line));
     } else {
-        $rover = [];
-        list($rover['x'], $rover['y'], $rover['orientation']) = explode(' ', trim($line));
+        $roverInput = [];
+        list($roverInput['x'], $roverInput['y'], $roverInput['orientation']) = explode(' ', trim($line));
         $line = fgets($handle);
-        $rover['instructions'] = str_split(trim($line));
-        $roversInput[] = $rover;
+        $roverInput['instructions'] = str_split(trim($line));
+        $roversInputs[] = $roverInput;
+    }
+}
+
+foreach ($roversInputs as $roverInput) {
+    $rover = new Rover(
+        new Plateau($plateauSize['x'], $plateauSize['y']),
+        $roverInput['x'],
+        $roverInput['y'],
+        $roverInput['orientation'],
+        $roverInput['instructions']
+    );
+    try {
+        $rover->executeInstructions();
+        print $rover->getInfo() . "\n";
+    } catch (Exception $e) {
+        print "Error occurred: " . $e->getMessage();
     }
 }
