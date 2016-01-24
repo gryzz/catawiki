ad4dc5a9427347d4853bcbf24ab91308118584c8
Igorko <gryzzbox@gmail.com>
2016-01-24 17:37:34 +0200

    Added more tests.

--- tests/Rover.php
+++ tests/Rover.php
@@ -21,8 +21,28 @@ class RoverTest extends PHPUnit_Framework_TestCase {
         $rover = new Rover($plateau, 1, 2, Plateau::NORTH, ['L','M','L','M','L','M','L','M','M']);
         $rover->executeInstructions();
         $this->assertEquals('1 3 N', $rover->getInfo());
+
+
         $rover = new Rover($plateau, 3, 3, Plateau::EAST, ['M', 'M', 'R', 'M', 'M', 'R', 'M', 'R', 'R', 'M']);
         $rover->executeInstructions();
         $this->assertEquals('5 1 E', $rover->getInfo());
+
+        $rover = new Rover($plateau, 5, 5, Plateau::SOUTH, ['M', 'M', 'M', 'M', 'M', 'R', 'M', 'M', 'M', 'M', 'M']);
+        $rover->executeInstructions();
+        $this->assertEquals('0 0 W', $rover->getInfo());
+
+        $rover = new Rover($plateau, 0, 0, Plateau::EAST, ['M', 'M', 'M', 'M', 'M', 'L', 'M', 'M', 'M', 'M', 'M']);
+        $rover->executeInstructions();
+        $this->assertEquals('5 5 N', $rover->getInfo());
+
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testExecuteInstructionException () {
+        $plateau = new Plateau(5,5);
+        $rover = new Rover($plateau, 5, 5, Plateau::SOUTH, ['M', 'M', 'M', 'M', 'M', 'L', 'M', 'M', 'M', 'M', 'M']);
+        $rover->executeInstructions();
     }
 }
